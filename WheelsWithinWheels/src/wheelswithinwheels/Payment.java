86ea2dc3da2c56edce718bac1b4f741be3908bb7
Gideon_mac <Gideon_mac@gideons-air.newton.edu>
2019-02-05 10:54:56 -0500

    Here's my stuff!

---
+++ WheelsWithinWheels/src/wheelswithinwheels/Payment.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheelswithinwheels;
+import java.util.Date;
+
+/**
+ *
+ * @author Gideon_mac
+ */
+public class Payment {
+    final int amount;
+    final Date date;
+    final String name;
+}

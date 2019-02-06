66919ce12cb38ef8c071ffbc2be0be98324afcc7
Gideon_mac <Gideon_mac@gideons-air.newton.edu>
2019-02-06 08:59:25 -0500

    Not much relevant stuff here at the moment.

--- WheelsWithinWheels/src/wheelswithinwheels/WheelsWithinWheels.java
+++ WheelsWithinWheels/src/wheelswithinwheels/WheelsWithinWheels.java
@@ -6,15 +6,16 @@ public class WheelsWithinWheels {
     public ArrayList<Order> orders = new ArrayList<Order>();
     public ArrayList<Customer> customers = new ArrayList<Customer>();
     
-    /*protected String saveToStulin () {
+    protected String saveToStulin () {
         String output = "";
         for (RepairPrice row : priceTable.getArray()) {
             output += "addrp " + row.brand + " " + row.tier + " " + row.price + " " + row.days + "\n";
         }
-        for (Order order: orders) {
-            
-        }
-    }*/
+        /*for (Order order: orders) {
+            output += "addo " + order.customer + " " + order.date + " " +  + " " +  + " " +  + " " +  +"\n";
+        }*/ //We need to store price data in here somewhere or update the repair prices every single time
+        return null;
+    }
     
     protected void reset () {
         bikeShop = new WheelsWithinWheels();

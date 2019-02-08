0eaa2a8733d6e439a37b7ad74312d2fa320e41ec
Gideon_mac <Gideon_mac@gideons-air.newton.edu>
2019-02-08 13:08:56 -0500

    I added some stuff

--- WheelsWithinWheels/src/wheelswithinwheels/UI.java
+++ WheelsWithinWheels/src/wheelswithinwheels/UI.java
@@ -1,9 +1,28 @@
 package wheelswithinwheels;
 
-/**
- *
- * @author Gideon_mac
- */
 public class UI {
+    public void parseline (String lineIn) {
+        String[] args = split(lineIn);
+        
+        switch (args[0]) {
+            case "help":
+                help(args);
+                break;
+            
+        }
+    }
     
+    public String[] split (String lineIn) {
+        String[] args = lineIn.split("\\s+");
+        while (args.length > 0 && args[0].equals("")) {
+            String[] newArray = new String[args.length - 1];
+            System.arraycopy(args, 1, newArray, 0, newArray.length);
+            args = newArray;
+        }
+        return args;
+    }
+    
+    public void help (String[] args) {
+        
+    }
 }
